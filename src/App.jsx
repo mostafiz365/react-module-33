d86037ffb8575ef3c73018e423f66af30cac7847
mostafiz365 <rahmanmostafiz365@gmail.com>
2026-03-15 12:46:28 +0600

    add some react function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
+import { Suspense } from 'react';
 import './App.css'
-import Countries from './components/Countries/Countries'
+import Countries from './components/Countries/Countries';
+
+
+const fetchCountries = async() => {
+  const res = await fetch('https://openapi.programming-hero.com/api/all');
+  return res.json();
+}
 
 function App() {
+  const countriesPromise = fetchCountries();
 
   return (
     <>
-        <Countries></Countries>
+        <Suspense fallback={<h3>All Countries data are Loading....</h3>}>
+          <Countries countriesPromise={countriesPromise}></Countries>
+        </Suspense>
     </>
   )
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import Country from '../Country/Country';
+import './countries.css'
 
-const Countries = () => {
+const Countries = ({countriesPromise}) => {
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+    const handelVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handelVisitedFlags = (flag) => {
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+
+    }
+
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
+
     return (
         <div>
-            <h2>In The Counties</h2>
+            <h2>In The Counties {countries.length}</h2>
+
+            <h3>Total Visited Country: {visitedCountries.length}</h3>
+            <h3>Total Visited Flags: {visitedFlags.length}</h3>
+            <div className='flag-size'>
+                {
+                    visitedFlags.map(flag => <img src={flag}></img>)
+                }
+            </div>
+
+            <div className='countries'>
+                {
+                countries.map(country => <Country key={country.cca3.cca3} country={country} handelVisitedCountries={handelVisitedCountries} handelVisitedFlags={handelVisitedFlags}></Country>)
+            }
+            </div>
+
         </div>
     );
 };
